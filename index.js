38bbe62ebc424e181eff7a79e214f59cdc8147e0
Abdullah9747 <abdullahfaisal9747@gamil.com>
2024-04-22 16:00:34 +0500

    email verify

--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const cars = require('./models');
 const offers = require('./offers');
 const path = require('path')
 
-
 app.set('view engine', 'ejs');
 app.set('views', './views')
 app.use(express.static(path.join(__dirname, 'Assets')));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
     res.render('index', { cars, offers })
 })
+
 app.get("/login", (req, res) => {
     res.render('login')
 })
+
 app.get("/signup", (req, res) => {
     res.render('signup')
 })
 
 app.use(bodyParser.json());
-
 
 app.post('/signup', (req, res) => {
     console.log("Received sign-up data:", req.body);
@@ -33,16 +34,15 @@ app.post('/signup', (req, res) => {
     console.log('First Name:', first_name);
     console.log('Last Name:', last_name);
 
-    // Send a response back to the client with the processed data
-    res.status(200).json({
-        message: 'User signed up successfully',
-        first_name: first_name,
-        last_name: last_name
-    });
+    // Redirect to email_verify route passing email as query parameter
+    res.redirect(`/email_verify?email=${email}`);
 });
 
-
+app.get("/email_verify", (req, res) => {
+    const { email } = req.query;
+    res.render('email_verify', { email });
+});
 
 app.listen(5000, () => {
     console.log("server started at http://localhost:5000");
-})
+});
