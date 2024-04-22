a5561bd8a3ace3b9627599c74ce8ef02cd76f38c
omparkash <parkashom.1215@gmail.com>
2024-04-22 10:57:39 +0500

    Changes to POST API made

--- public/js/signup.js
+++ public/js/signup.js
@@ -15,7 +15,47 @@ function validateForm() {
     var lastName = document.getElementById("last-name").value;
     var form = document.getElementById("signupForm");
     if (form.checkValidity()) {
-        alert(`${salutation} ${firstName} ${lastName}, welcome to the Porsche Club!`);
+        document.getElementById('signupForm').addEventListener('submit', async (e) => {
+            e.preventDefault();
+        
+            const formData = new FormData(e.target);
+            const formObject = {};
+        
+            formData.forEach((value, key) => {
+                formObject[key] = value;
+            });
+        
+            try {
+                const response = await fetch('/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formObject)
+                });
+        
+                if (!response.ok) {
+                    throw new Error('Failed to submit form');
+                }
+        
+                const data = await response.json();
+                console.log('Server Response:', data);
+        
+                if (data.first_name && data.last_name) {
+                    console.log('Received First Name:', data.first_name);
+                    console.log('Received Last Name:', data.last_name);
+                    alert(`${data.first_name} ${data.last_name}, welcome to the Porsche Club!`);
+                } else {
+                    console.log('First Name or Last Name not provided by server');
+                }
+        
+                alert(data.message);
+            } catch (error) {
+                console.error('Error:', error.message);
+                alert('An error occurred while signing up');
+            }
+        });
+        
     } else {
         form.querySelectorAll(':invalid').forEach((elem) => {
             elem.classList.add('is-invalid');
@@ -23,33 +63,5 @@ function validateForm() {
     }
 }
 
-document.getElementById('signupForm').addEventListener('submit', async (e) => {
-    
-    e.preventDefault();
-    const formData = new FormData(e.target); 
-    const formObject = {};
-    formData.forEach((value, key) => {
-        formObject[key] = value;
-    });
-    
-    
-    try {
-        const response = await fetch('/signup', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(formObject)
-        });
 
-        if (!response.ok) {
-            throw new Error('Failed to submit form');
-        }
 
-        const data = await response.json();
-        alert(data.message); 
-    } catch (error) {
-        console.error('Error:', error.message);
-        alert('An error occurred while signing up');
-    }
-});
